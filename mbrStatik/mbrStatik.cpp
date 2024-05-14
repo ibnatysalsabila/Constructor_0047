8f7d053ba1cd5c489e3ba53e7fc316a1b3642b62
ibnatysalsabila <salsajpr01@gmail.com>
2024-05-14 18:03:32 +0700

    memasukan static variabel

--- mbrStatik/mbrStatik.cpp
+++ mbrStatik/mbrStatik.cpp
@@ -2,3 +2,20 @@
 #include <string>
 using namespace std;
 
+class mahasiswa
+{
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+
+	void printAll();
+	mahasiswa(string pnama)
+	{
+		nama = pnama;
+		setID();
+	}
+};
+
